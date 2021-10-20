c57de3e066456e37d28cbeb402f50de36ed1f1d6
Israel Rodrigues de Jesus <israel.jesus2@zup.com.br>
2021-10-19 21:49:56 -0300

    clean main class and create SeedGenerator

--- src/main/kotlin/br/com/shortify/shortifymanager/ShortifyManagerApplication.kt
+++ src/main/kotlin/br/com/shortify/shortifymanager/ShortifyManagerApplication.kt
@@ -1,40 +1,10 @@
 package br.com.shortify.shortifymanager
 
-import br.com.shortify.shortifymanager.domain.UrlKey
-import br.com.shortify.shortifymanager.repository.UrlKeyRepository
-import org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-const val LENGTH = 5
-
 @SpringBootApplication
-class ShortifyManagerApplication : CommandLineRunner {
-
-    @Autowired
-    lateinit var urlKeyRepository : UrlKeyRepository
-
-    val logger = LoggerFactory.getLogger(javaClass)
-
-    override fun run(vararg args: String?) {
-        val shouldGenerateSomeKeys = true
-
-        if(shouldGenerateSomeKeys){
-            logger.info("Creating new url keys")
-            val keys = mutableSetOf<String>()
-
-            for (x in 0..50){
-                keys.add(randomAlphanumeric(LENGTH).lowercase())
-            }
-
-            logger.info("${keys.size} Generated keys = $keys")
-            urlKeyRepository.saveAll(keys.map { UrlKey(it) })
-        }
-    }
-}
+class ShortifyManagerApplication
 
 fun main(args: Array<String>) {
     runApplication<ShortifyManagerApplication>(*args)
---
+++ src/main/kotlin/br/com/shortify/shortifymanager/seed/KeySeedGenerator.kt
@@ -0,0 +1,32 @@
+package br.com.shortify.shortifymanager.seed
+
+import br.com.shortify.shortifymanager.domain.UrlKey
+import br.com.shortify.shortifymanager.repository.UrlKeyRepository
+import org.apache.commons.lang3.RandomStringUtils
+import org.slf4j.LoggerFactory.getLogger
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+
+const val LENGTH = 5
+
+@Component
+class KeySeedGenerator(
+    val urlKeyRepository: UrlKeyRepository,
+    @Value("\${keys-to-generate-at-start}") val urlKeysGeneratedAtStart: Long
+) : CommandLineRunner {
+
+    val logger = getLogger(javaClass)
+
+    override fun run(vararg args: String?) {
+            logger.info("Creating new url keys")
+            val keys = mutableSetOf<String>()
+
+            for (x in 1..urlKeysGeneratedAtStart) {
+                keys.add(RandomStringUtils.randomAlphanumeric(LENGTH).lowercase())
+            }
+
+            logger.info("${keys.size} Generated keys = $keys")
+            urlKeyRepository.saveAll(keys.map { UrlKey(it) })
+    }
+}
